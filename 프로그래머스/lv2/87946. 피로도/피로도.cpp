bac269ba59afec61107c5119f175aa8c7d3474fe
Kim YeonMo <ymthebest57@gmail.com>
2022-11-20 16:45:30 +0900

    [level 2] Title: 피로도, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = -1;
+    
+    sort(dungeons.begin(), dungeons.end());
+    
+    do{
+        int count = 0;
+        int temp = k;
+        
+        for(int i=0; i<dungeons.size(); i++){
+             if(temp>=dungeons[i][0]){
+                 temp-=dungeons[i][1];
+                 count++;
+             }
+            else {
+                break;
+            }
+        }
+        answer = max(count, answer);
+        
+    }while (next_permutation(dungeons.begin(), dungeons.end()));
+    
+    return answer;
+}
