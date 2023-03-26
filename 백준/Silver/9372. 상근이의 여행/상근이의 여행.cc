d275edf75b4e198603a27e7bb9e107dafb8dc3a3
Kim YeonMo <ymthebest57@gmail.com>
2023-03-26 21:41:22 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int t, n, m;
+
+int main() {
+	cin >> t;
+	while (t--) {
+		cin >> n >> m;
+		for (int i = 0; i < m; i++) {
+			int a, b;
+			cin >> a >> b;
+		}
+		
+		cout << n - 1 << endl;
+	}
+}
