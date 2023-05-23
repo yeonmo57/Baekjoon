b9874a738bb37a4f1c737cd114f9cf6f9d3fc45e
Kim YeonMo <ymthebest57@gmail.com>
2023-05-23 19:09:05 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cc
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cc
@@ -1,35 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int right_x;
-int right_y;
-int left_x;
-int left_y;
-int cnt = 0;
-bool graph[101][101];
+int main() {
+    int graph[101][101] = {0}; // 직사각형의 면적을 저장할 배열
 
-void calculate_area(int _left_x, int _left_y, int _right_x, int _right_y)
-{
-	for (int i = _left_x; i < _right_x; i++)
-	{
-		for (int j = _left_y; j < _right_y; j++)
-		{
-			if (graph[i][j] == true) 
-			{
-				continue;
-			}
-			graph[i][j] = true; 
-			cnt++;
-		}
-	}
-}
-int main() 
-{
-	for (int i = 0; i < 4; i++)
-	{
-		cin >> left_x >> left_y >> right_x >> right_y;
-		calculate_area(left_x, left_y, right_x, right_y);
-	}
-	cout << cnt << endl;
-	return 0;
+    // 직사각형 입력받고 면적 표시
+    for (int i = 0; i < 4; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
+                graph[x][y] = 1;
+            }
+        }
+    }
+
+    // 면적 계산
+    int area = 0;
+    for (int x = 0; x < 101; x++) {
+        for (int y = 0; y < 101; y++) {
+            if (graph[x][y] == 1) {
+                area++;
+            }
+        }
+    }
+
+    cout << area << endl;
+
+    return 0;
 }
