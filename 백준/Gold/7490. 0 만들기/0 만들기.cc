9493facbbe72b09942bc7893a0afa1f82792e7fe
Kim YeonMo <ymthebest57@gmail.com>
2023-06-05 16:57:38 +0900

    Update 0 만들기.cc

--- 백준/Gold/7490. 0 만들기/0 만들기.cc
+++ 백준/Gold/7490. 0 만들기/0 만들기.cc
@@ -1,34 +1,54 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int N;
-void solve(int sum, int sign, int num, int n, string str)
+
+int testcase, num;
+
+vector<string> ans;
+
+void solve(int target, int sum, int sign, int prev, int idx, string expression)
 {
-    if (n == N)
+    if (idx == target)
     {
-        sum += (num * sign);
+        sum += (prev * sign);
         if (sum == 0)
         {
-            cout << str << '\n';
+            ans.push_back(expression);
         }
+        return;
     }
     else
     {
-        solve(sum, sign, num * 10 + (n + 1), n + 1, str + ' ' + char(n + 1 + '0'));
-        solve(sum + (sign*num), 1, n + 1, n + 1, str + '+' + char(n + 1 + '0'));
-        solve(sum + (sign*num), -1, n + 1, n + 1, str + '-' + char(n + 1 + '0'));
+        // +
+        solve(target, sum + (prev * sign), 1, idx + 1, idx + 1, expression + '+' + to_string(idx + 1));
+
+        // -
+        solve(target, sum + (prev * sign), -1, idx + 1, idx + 1, expression + '-' + to_string(idx + 1));
+
+        // 공백으로 계산
+        solve(target, sum, sign, prev * 10 + (idx + 1), idx + 1, expression + ' ' + to_string(idx + 1));
     }
 }
+
 int main()
 {
-    int test_case;
-    char test;
-    scanf("%d", &test_case);
-    for (int i = 0; i < test_case; i++)
+    cin >> testcase;
+    while (testcase--)
     {
-        scanf("%d", &N);
-        solve(0, 1, 1, 1, "1");// sum, sign, num, n, str
-        printf("\n");
+
+        cin >> num;
+        solve(num, 0, 1, 1, 1, "1");
+
+        sort(ans.begin(), ans.end());
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << endl;
+        }
+        cout << endl;
+        ans.clear();
     }
+
     return 0;
 }
