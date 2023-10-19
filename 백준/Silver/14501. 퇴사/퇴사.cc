5bae5acb7aaf0fd34765f6a63d3316fe30710471
Kim YeonMo <ymthebest57@gmail.com>
2023-10-19 22:07:20 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,39 +1,40 @@
 #include <iostream>
- 
+
 using namespace std;
- 
-int t[1000] = { 0, };
-int p[1000] = { 0, };
-int dp[1000] = { 0, };
- 
+
 int max(int x, int y)
 {
-    return x > y ? x : y;
+	return x > y ? x : y;
 }
- 
+
 int main()
 {
-    int N;
-    int next;
- 
-    cin >> N;
- 
-    for (int i = 1;i <= N;i++) {
-        cin >> t[i] >> p[i];
-    }
- 
-    for (int i = N;i > 0;i--) {
-        next = i + t[i];
-        if (next > N + 1) {
-            dp[i] = dp[i + 1];
-        }
-        else {
-            dp[i] = max(dp[i + 1], dp[next] + p[i]);
-        }
- 
-    }
- 
-    cout << dp[1] << endl;
- 
-    return 0;
+	int n;
+	int next;
+
+	int t[1000] = { 0, };
+	int p[1000] = { 0, };
+	int dp[1000] = { 0, };
+	
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> t[i] >> p[i];
+	}
+
+	for (int i = n; i > 0; i--)
+	{
+		next = i + t[i];
+		if (next - 1> n) 
+		{
+			dp[i] = dp[i + 1];
+		}
+		else
+		{
+			dp[i] = max(dp[i + 1], dp[next] + p[i]);
+		}
+	}
+
+	cout << dp[1] << endl;
+	return 0;
 }
