5f385195fa00887779bd108e12987bfbe025ed1c
Kim YeonMo <ymthebest57@gmail.com>
2023-10-26 23:57:58 +0900

    [level 2] Title: 전화번호 목록, Time: 743.80 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+bool solution(vector<string> phone_book)
+{
+    map<string,int> m;
+    for(int i=0;i<phone_book.size();i++) m[phone_book[i]]++;
+    for(int i=0;i<phone_book.size();i++)
+        for(int j=1;j<phone_book[i].size();j++)
+            if(m[phone_book[i].substr(0,j)]) return false;
+
+    return true;
+}
