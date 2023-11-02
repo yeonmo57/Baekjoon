5011788b34b4a4b3021574ebe078ac4870c39b9d
Kim YeonMo <ymthebest57@gmail.com>
2023-11-02 23:05:05 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -3,36 +3,51 @@
 #include <algorithm>
 using namespace std;
 
-int main()
+int a[100005];
+int n;
+
+int binarysearch(int target)
 {
-	int n, m;
-	vector<int> v;
-
-	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++)
+	int st = 0;
+	int en = n - 1;
+	while (st <= en)
 	{
-		int temp;
-		scanf("%d", &temp);
-		v.push_back(temp);
-	}
-
-	sort(v.begin(), v.end());
-
-	scanf("%d", &m);
-
-	for (int i = 0; i < m; i++)
-	{
-		int target;
-		scanf("%d", &target);
-
-		if (binary_search(v.begin(), v.end(), target))
+		int mid = (st + en) / 2;
+		if (a[mid] < target)
 		{
-			printf("1\n");
+			st = mid + 1;
+		}
+		else if (a[mid] > target)
+		{
+			en = mid - 1;
 		}
 		else
 		{
-			printf("0\n");
+			return 1;
 		}
+	}
+	return 0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	sort(a, a + n);
+
+	int m;
+	cin >> m;
+	while (m--)
+	{
+		int t;
+		cin >> t;
+		cout << binarysearch(t) << '\n';
 	}
 }
