57fb3f7a4ef042e4778ed6722e3e0c785501fd4b
Kim YeonMo <ymthebest57@gmail.com>
2023-12-05 01:39:38 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 3956 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,44 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
- 
-vector<int> graph[32001];
-int inDegree[32001];
-int N, M;
- 
-void TopologicalSort(void){
-    queue<int> q;
-    
-    for(int i = 1; i <= N; i++)
-        if(!inDegree[i])
-            q.push(i);
-        
-    while(!q.empty()){
-        int cur = q.front();
-        q.pop();
-        cout << cur << ' ';
-        for(int i = 0; i < graph[cur].size(); i++){
-            inDegree[graph[cur][i]]--;
-            if(!inDegree[graph[cur][i]])
-                q.push(graph[cur][i]);
-        }
-    }
+
+vector<int> adj[32001];
+int deg[32001];
+int n, m;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	while (m--)
+	{
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+		deg[b]++;
+	}
+
+	queue<int> q;
+	for (int i = 1; i <= n; i++)
+	{
+		if (deg[i] == 0)
+		{
+			q.push(i);
+		}
+	}
+
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+		cout << cur << ' ';
+		for (int nxt : adj[cur])
+		{
+			deg[nxt]--;
+			if (deg[nxt] == 0)
+			{
+				q.push(nxt);
+			}
+		}
+	}
 }
- 
-int main(int argc, const char * argv[]) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
-    cin >> N >> M;
-    for(int i = 0; i < M; i++){
-        int a, b;
-        cin >> a >> b;
-        graph[a].push_back(b);
-        inDegree[b]++;
-    }
-    
-    TopologicalSort();
-    
-    return 0;
-}
- 
